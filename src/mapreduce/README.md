9ebcf00f206674ba5d39edbee18a0ebef6694982
jchen45 <chenjb90@gmail.com>
2020-06-07 13:03:00 -0400

    Update the readme of lab1

---
+++ src/mapreduce/README.md
@@ -0,0 +1,14 @@
+## Notes for Lab1
+
+#### Commands
+
+**1. Testing**
+```
+go test -run Sequential
+```
+
+#### Reference
+1. http://www.cnblogs.com/YaoDD/p/6073794.html
+2. http://blog.csdn.net/bysui/article/details/52128221
+3. http://www.jianshu.com/p/bfb4aee7a827
+4. http://gaocegege.com/Blog/distributed%20system/ds-lab1
--- src/mapreduce/schedule.go
+++ src/mapreduce/schedule.go
@@ -1,6 +1,9 @@
 package mapreduce
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
 //
 // schedule() starts and waits for all tasks in the given phase (mapPhase
@@ -13,22 +16,59 @@ import "fmt"
 //
 func schedule(jobName string, mapFiles []string, nReduce int, phase jobPhase, registerChan chan string) {
 	var ntasks int
-	var nOther int // number of inputs (for reduce) or outputs (for map)
+	var nother int // number of inputs (for reduce) or outputs (for map)
 	switch phase {
 	case mapPhase:
 		ntasks = len(mapFiles)
-		nOther = nReduce
+		nother = nReduce
 	case reducePhase:
 		ntasks = nReduce
-		nOther = len(mapFiles)
+		nother = len(mapFiles)
 	}
 
-	fmt.Printf("Schedule: %v %v tasks (%d I/Os)\n", ntasks, phase, nOther)
+	fmt.Printf("Schedule: %v %v tasks (%d I/Os)\n", ntasks, phase, nother)
 
 	// All ntasks tasks have to be scheduled on workers. Once all tasks
 	// have completed successfully, schedule() should return.
 	//
 	// Your code here (Part III, Part IV).
 	//
+
+	/*
+		1. Get worker from registerChan
+		2. Call "DoTask" service using RPC for each task
+		3. Return the worker back to registerChan after using it
+		4. Do not return until all the tasks are finished
+	*/
+	var wg sync.WaitGroup
+	for i := 0; i < ntasks; i++ {
+		doTaskArgs := DoTaskArgs{
+			JobName:       jobName,
+			File:          mapFiles[i],
+			Phase:         phase,
+			TaskNumber:    i,
+			NumOtherPhase: nother}
+		wg.Add(1)
+		go func(doTaskArgs DoTaskArgs, registerChan chan string) {
+			// get one worker from registerChan if available, block if worker not available
+			worker := <-registerChan
+
+			callResult := call(worker, "Worker.DoTask", doTaskArgs, nil)
+
+			if callResult == false {
+				fmt.Printf("Schedule: failed to call Worker %s DoTask in RPC\n", worker)
+			}
+
+			// put worker back after using it
+			go func() {
+				registerChan <- worker
+			}()
+
+			wg.Done()
+			return
+		}(doTaskArgs, registerChan)
+	}
+	wg.Wait()
+
 	fmt.Printf("Schedule: %v done\n", phase)
 }
